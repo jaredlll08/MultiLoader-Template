509dda3840e21b247d295ac8609c018720f5e5c2
Tyler Hancock <darklime@live.ca>
2021-10-05 22:33:10 -0600

    Cleaned up example code, and added more relevant examples and documentation.

--- Forge/src/main/java/com/example/examplemod/ExampleMod.java
+++ Forge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,69 +1,33 @@
 package com.example.examplemod;
 
-import java.util.stream.Collectors;
-
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 
 @Mod(Constants.MOD_ID)
 public class ExampleMod {
     
     public ExampleMod() {
-        
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        
-        MinecraftForge.EVENT_BUS.register(this);
-    }
     
-    private void setup(final FMLCommonSetupEvent event) {
-        
-        Constants.LOG.info("HELLO FROM PREINIT");
-        Constants.LOG.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        // This method is invoked by the Forge mod loader when it is ready
+        // to load your mod. You can access Forge and Common code in this
+        // project.
+    
+        // Use Forge to bootstrap the Common mod.
+        Constants.LOG.info("Hello Forge world!");
         CommonClass.init();
-    }
     
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        
-        InterModComms.sendTo("examplemod", "helloworld", () -> {
-            Constants.LOG.info("Hello world from the MDK");
-            return "Hello world";
-        });
-    }
-    
-    private void processIMC(final InterModProcessEvent event) {
-        
-        Constants.LOG.info("Got IMC {}", event.getIMCStream()
-                .map(m -> m.messageSupplier().get())
-                .collect(Collectors.toList()));
-    }
-    
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        
-        Constants.LOG.info("HELLO from server starting");
-    }
-    
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            Constants.LOG.info("HELLO from Register Block");
-        }
+        // Some code like events require special initialization from the
+        // loader specific code.
+        MinecraftForge.EVENT_BUS.addListener(this::onItemTooltip);
         
     }
     
+    // This method exists as a wrapper for the code in the Common project.
+    // It takes Forge's event object and passes the parameters along to
+    // the Common listener.
+    private void onItemTooltip(ItemTooltipEvent event) {
+        
+        CommonClass.onItemTooltip(event.getItemStack(), event.getFlags(), event.getToolTip());
+    }
 }
