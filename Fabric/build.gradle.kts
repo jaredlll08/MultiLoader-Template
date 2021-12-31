35234aa0f7a66e61a2e0b425dbce63e1ea176f0e
Jared <jaredlll08@gmail.com>
2021-12-31 22:18:17 +0200

    Kotlin DSL

---
+++ Fabric/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    idea
+    `maven-publish`
+    id("fabric-loom") version "0.10-SNAPSHOT"
+}
+
+val minecraftVersion: String by project
+val fabricVersion: String by project
+val fabricLoaderVersion: String by project
+val modName: String by project
+val modId: String by project
+
+val baseArchiveName = "${modName}-fabric-${minecraftVersion}"
+
+base {
+    archivesName.set(baseArchiveName)
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${minecraftVersion}")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+    implementation(project(":Common"))
+}
+
+loom {
+    runs {
+        named("client") {
+            client()
+            configName = "Fabric Client"
+            ideConfigGenerated(true)
+            runDir("run")
+        }
+        named("server") {
+            server()
+            configName = "Fabric Server"
+            ideConfigGenerated(true)
+            runDir("run")
+        }
+    }
+}
+
+
+tasks.processResources {
+    from(project(":Common").sourceSets.main.get().resources)
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    source(project(":Common").sourceSets.main.get().allSource)
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${modName}" }
+    }
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifactId = baseArchiveName
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven("file://${System.getenv("local_maven")}")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        maven("https://repo.spongepowered.org/repository/maven-public/") {
+            name = "Sponge Snapshots"
+        }
+        maven("https://maven.minecraftforge.net") {
+            name = "Forge"
+        }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            // If we request Forge, actually give it the correct artifact.
+            if (requested.id.id == "net.minecraftforge.gradle") {
+                useModule("${requested.id}:ForgeGradle:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "MultiLoader"
+include("Common", "Fabric", "Forge")
