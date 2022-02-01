36367b2382b6e889a386b725590070f20859aee8
Jared <jaredlll08@gmail.com>
2022-02-01 23:24:59 +0200

    Add service loader example. Close #7

--- Common/src/main/java/com/example/examplemod/Constants.java
+++ Common/src/main/java/com/example/examplemod/Constants.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Constants {
 
-	public static final String MOD_ID = "multiloader";
-	public static final String MOD_NAME = "MultiLoader";
+	public static final String MOD_ID = "examplemod";
+	public static final String MOD_NAME = "Example Mod";
 	public static final Logger LOG = LogManager.getLogger(MOD_NAME);
 }
