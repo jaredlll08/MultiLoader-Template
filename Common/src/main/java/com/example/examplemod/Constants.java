a79e8e7bc26e7cb2671bdac6a75cce7330bc3645
Tyler Hancock <darklime@live.ca>
2022-03-01 21:26:33 -0700

    Update to 1.18.2

--- Common/src/main/java/com/example/examplemod/Constants.java
+++ Common/src/main/java/com/example/examplemod/Constants.java
@@ -1,11 +1,11 @@
 package com.example.examplemod;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Constants {
 
-	public static final String MOD_ID = "examplemod";
-	public static final String MOD_NAME = "Example Mod";
-	public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+	public static final String MOD_ID = "multiloader";
+	public static final String MOD_NAME = "Multi Loader Template";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
