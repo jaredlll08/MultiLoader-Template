8e8ebcd2f5b22e83edcb566ccc9620c787e066ea
Nik-Kot <42574700+Nik-Kot@users.noreply.github.com>
2023-05-17 23:03:15 +0300

    Update to Gradle 8.1.1, ForgeGradle can be made to work with it

--- Common/src/main/java/com/example/examplemod/CommonInit.java
+++ Common/src/main/java/com/example/examplemod/CommonInit.java
@@ -8,7 +8,7 @@ import net.minecraft.world.item.Items;
 // import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
 // common compatible binaries. This means common code can not directly use loader specific concepts such as Forge events
 // however it will be compatible with all supported mod loaders.
-public class CommonInit {
+public class  CommonInit {
 
     // The loader specific projects are able to import and use any code from the common project. This allows you to
     // write the majority of your code here and load it from your loader specific projects. This example has some
