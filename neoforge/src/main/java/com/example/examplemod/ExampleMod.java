b7e9b83b7d42fa5d5388103b8dcac3f51e63ed54
GirafiStudios <faj10@msn.com>
2023-12-30 23:20:52 +0100

    1.20.4 changes

--- neoforge/src/main/java/com/example/examplemod/ExampleMod.java
+++ neoforge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,12 +1,13 @@
 package com.example.examplemod;
 
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
 public class ExampleMod {
 
-    public ExampleMod() {
+    public ExampleMod(IEventBus eventBus) {
 
         // This method is invoked by the NeoForge mod loader when it is ready
         // to load your mod. You can access NeoForge and Common code in this
